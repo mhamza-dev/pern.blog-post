190393f94ae8d83a590ca48bcf3597c4dce59fd7
mhamza-dev <hamzarakha786@gmail.com>
2024-08-08 16:50:58 +0500

    Changelog: - Implemented Regisration of a User - Implemented Authentication of User

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = process.env.HostPort || 5000
+const port = process.env.HOST_PORT || 5000;
+const userRoutes = require("./routes/userRoutes");
+
+app.use(express.json())
+app.use("/users", userRoutes);
 
 app.listen(port, () => {
   console.log(`${port} Connected!`);
