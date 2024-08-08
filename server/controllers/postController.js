411dc19b30d8d9984f69a637c0bc54b6f579abf3
mhamza-dev <hamzarakha786@gmail.com>
2024-08-08 20:00:29 +0500

    Changelog: - Updated post body type in schema - created routes for posts - created crud for respective routes - added middleware of posts, categories in index.js - removed extra console.log()

---
+++ server/controllers/postController.js
@@ -0,0 +1,61 @@
+const { Post, User, Comment } = require("../models");
+
+const getAllPosts = async (_req, resp) => {
+  try {
+    const posts = await Post.findAll({
+      include: [
+        { model: User, as: "user" },
+        { model: Comment, as: "comments" },
+      ],
+    });
+    resp.status(200).json(posts);
+  } catch (error) {
+    resp.status(500).json({ error: error.message });
+  }
+};
+
+const getPostById = async (req, resp) => {
+  try {
+    const post = await Post.findByPk(req.params.id, {
+      include: [
+        { model: User, as: "user" },
+        { model: Comment, as: "comments" },
+      ],
+    });
+    if (!post) return resp.status(404).json({ message: "Post not found" });
+    resp.status(200).json(post);
+  } catch (error) {
+    resp.status(500).json({ error: error.message });
+  }
+}
+
+const createPost = async (req, resp) => {
+  try {
+    const post = await Post.create({...req.body, userId: req.user.data.id});
+    resp.status(200).json(post);
+  } catch (error) {
+    resp.status(500).json({ error: error.message });
+  }
+}
+
+const deletePost = async (req, resp) => {
+  try {
+    await Post.destroy({ where: { id: req.params.id } });
+    resp.status(200).json("post has been deleted");
+  } catch (error) {
+    resp.status(500).json({ error: error.message });
+  }
+};
+
+const updatePost = async (req, resp) => {
+  try {
+    await Post.update(req.body, {
+      where: { id: req.params.id },
+    });
+    resp.status(200).json("post has been updated");
+  } catch (error) {
+    resp.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getAllPosts, getPostById, createPost, deletePost, updatePost };
--- server/index.js
+++ server/index.js
@@ -3,9 +3,13 @@ const express = require('express')
 const app = express()
 const port = process.env.HOST_PORT || 5000;
 const userRoutes = require("./routes/userRoutes");
+const postRoutes = require("./routes/postRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
 
 app.use(express.json())
 app.use("/users", userRoutes);
+app.use('/categories', categoryRoutes)
+app.use("/posts", postRoutes);
 
 app.listen(port, () => {
   console.log(`${port} Connected!`);
